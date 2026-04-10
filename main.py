6dc67b99cba42f50c0e8436a72f43d8d374914ee
takxz <corentin.leys@gmail.com>
2026-04-10 19:32:01 +0200

    feat: simulation de jeu dans le main

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ from src.paquet import Paquet
 def main():
     carte = Carte("trois", "PIQUE")
     print(carte)
-    print(carte.points())
+    print(f"Points: {carte.points()}")
 
     paquet = Paquet()
     paquet.shuffle()
     paquet.cut()
     carte_piochee = paquet.piocher()
-    print(carte_piochee)
-    print(carte_piochee.points())
-    
-
-
+    print(f"Carte piochée: {carte_piochee}")
+    print(f"Points de la carte piochée: {carte_piochee.points()}")
+    print(f"Nombre de cartes restantes dans le paquet: {len(paquet.cartes)}")
+    mains = paquet.distribuer(5, 4)
+    for i, main in enumerate(mains):
+        print(f"Main du joueur {i + 1}:")
+        for carte in main:
+            print(f"{carte}")
 
 if __name__ == "__main__":
     main()
