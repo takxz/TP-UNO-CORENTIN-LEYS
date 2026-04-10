8e0d4cd90f101b092d13cb9a5d757945d68cf910
takxz <corentin.leys@gmail.com>
2026-04-10 15:38:58 +0200

    fix: grace au test, j'ai vu que les cartes n'étaient pas ordonnée comme il fallait + ajout du shuffle

--- src/paquet.py
+++ src/paquet.py
@@ -1,15 +1,14 @@
 from src.carte import Carte
 from src.valeur import Valeur
 from src.couleur import Couleur
-
+import random
 
 class Paquet:
     def __init__(self):
         self.cartes = []
-        for valeur in Valeur:
-            for couleur in Couleur:
+        for couleur in Couleur:
+            for valeur in Valeur:
                 self.cartes.append(Carte(valeur, couleur))
     
-
-
-
+    def shuffle(self):
+        random.shuffle(self.cartes)
--- tests/test_paquet.py
+++ tests/test_paquet.py
@@ -4,3 +4,21 @@ from src.paquet import Paquet
 def test_initialisation():
     paquet = Paquet()
     assert len(paquet.cartes) == 52
+
+def test_ordre_carte():
+    paquet = Paquet()
+    assert repr(paquet.cartes[0]) == "Carte AS de COEUR"
+    assert repr(paquet.cartes[1]) == "Carte 2 de COEUR"
+    assert repr(paquet.cartes[12]) == "Carte ROI de COEUR"
+    assert repr(paquet.cartes[13]) == "Carte AS de CARREAU"
+    assert repr(paquet.cartes[25]) == "Carte ROI de CARREAU"
+    assert repr(paquet.cartes[26]) == "Carte AS de PIQUE"
+    assert repr(paquet.cartes[38]) == "Carte ROI de PIQUE"
+    assert repr(paquet.cartes[39]) == "Carte AS de TREFLE"
+    assert repr(paquet.cartes[51]) == "Carte ROI de TREFLE"
+
+def test_shuffle():
+    paquet = Paquet()
+    cartes_avant_shuffle = paquet.cartes.copy()
+    paquet.shuffle()
+    assert paquet.cartes != cartes_avant_shuffle
